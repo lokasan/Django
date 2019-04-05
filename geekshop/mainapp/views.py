a2732d342881b6f19fb0c50e3feb70ae491bd411
lokasan <pololopo@yandex.ru>
2019-04-05 14:53:08 +0300

    4

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -4,23 +4,23 @@ from .models import Product, ProductCategory
 
 def main(request):
     prod_cat = ProductCategory.objects.all()
-    return render(request, 'mainapp/index.html', context={'menu':{'main': 'Главная', 'catalogue': 'Каталог', 'contacts': 'Контакты'}, 'page': 'Главная', 'prod_cat': prod_cat})
+    return render(request, 'mainapp/index.html', context={'user': request.user, 'menu': {'main': 'Главная', 'catalogue': 'Каталог', 'contacts': 'Контакты'}, 'page': 'Главная', 'prod_cat': prod_cat})
     
 
-def catalogue(request):
+def catalogue(request, pk=None):
     products = Product.objects.all()
     prod_cat =ProductCategory.objects.all()
-    return render(request, 'mainapp/catalogue/index.html', context={'menu': {'main': 'Главная', 'catalogue': 'Каталог', 'contacts': 'Контакты'}, 'page': 'Каталог', 'products': products, 'prod_cat': prod_cat})
+    return render(request, 'mainapp/catalogue/index.html', context={'user': request.user, 'menu': {'main': 'Главная', 'catalogue': 'Каталог', 'contacts': 'Контакты'}, 'page': 'Каталог', 'products': products, 'prod_cat': prod_cat})
     
 
 def contacts(request):
     prod_cat = ProductCategory.objects.all()
-    return render(request, 'mainapp/contacts/index.html', context={'placeholder': {'name': 'ivan', 'email': 'mail@gmail.com', 'theme': 'покупка монитора'}, 'prod_cat': prod_cat, 'menu': {'main': 'Главная', 'catalogue': 'Каталог', 'contacts': 'Контакты'}, 'page': 'Контакты'})
+    return render(request, 'mainapp/contacts/index.html', context={'user': request.user, 'placeholder': {'name': 'ivan', 'email': 'mail@gmail.com', 'theme': 'покупка монитора'}, 'prod_cat': prod_cat, 'menu': {'main': 'Главная', 'catalogue': 'Каталог', 'contacts': 'Контакты'}, 'page': 'Контакты'})
 
 
 def monitor(request):
     prod_cat = ProductCategory.objects.all()
-    return render(request, r'mainapp/catalogue/monitor/Eizo ColorEdge CG318-4K/CG318-4K.html', context={'menu':{'main': 'Главная', 'catalogue': 'Каталог', 'contacts': 'Контакты'}, 'prod_cat': prod_cat})
+    return render(request, r'mainapp/catalogue/monitor/Eizo ColorEdge CG318-4K/CG318-4K.html', context={'user': request.user, 'menu': {'main': 'Главная', 'catalogue': 'Каталог', 'contacts': 'Контакты'}, 'prod_cat': prod_cat})
 # Create your views here.
 
 # Create your views here.
